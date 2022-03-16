e3adb5d2a6d54871439f1596ead50705e5ca8583
Home-Xubuntu <durgeshsr@gmail.com>
2022-03-15 20:30:18 -0700

    adding project files using command line on xubuntu machine

---
+++ VoiceToTextDLL/VoiceToTextDLL/Class1.cs
@@ -0,0 +1,53 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public async Task Main()
+        {
+            await RecognizeSpeechAsync();
+        }
+
+        public async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("d6913ab6a521413bb52dabc21edbf3a8", "centralindia");
+
+            using (var audioInput = AudioConfig.FromWavFileInput("D:\\durgesh.ramani\\My Downloads\\HDFCPhoneBanking.wav"))
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                Console.WriteLine("Recognizing first result...");
+                var result = await recognizer.RecognizeOnceAsync();
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                        Console.WriteLine($"We recognized: {result.Text}");
+                        break;
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
